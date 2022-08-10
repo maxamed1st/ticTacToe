aba322398b01b7e623bae4bc7c5dd5e439978941
maxamed1st <maxamed1st@icloud.com>
2022-08-10 14:48:56 +0200

    set up skeleton files

---
+++ ticTacToe.js
@@ -0,0 +1,12 @@
+const gameBoard = (function() {
+    //Module for the game board
+})();
+const displayController = (function() {
+    //Module to control the display
+})();
+const players = function(name1, name2) {
+    //Factory function for players
+}
+const main = function() {
+    //Main function to bring together the previouse function and interconnect them!
+}
