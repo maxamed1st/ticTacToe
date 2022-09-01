a2cc9e86ceb7df51b0ecd0bfcd29cec45a116cbd
maxamed1st <maxamed1st@icloud.com>
2022-09-01 22:19:47 +0100

    build logic to let players add marks to the gameboard

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,36 +4,48 @@ const formContainer = document.getElementById("formContainer");
 const playerNames = document.getElementById("playerNames");
 const firstPlayerDiv = document.getElementById("firstPlayerDiv");
 const secondPlayerDiv = document.getElementById("secondPlayerDiv");
+let firstPlayer = true;
 const gameBoard = (function() {
     //Module for the game board
     gameArray = [];
     let firstPlayerTurn = true;
-    const gridCellCallback = function(e) {
-        //get players marker!!!!
-        let id = e.target.id
+    const gridCellCallback = function(playerTwo, e) {
+        //get players marker and add to gridcell on click
+        //this = playerOne
+        const markerX = this.getMarker();
+        const markerO = playerTwo.getMarker();
+        const id = e.target.id
         if (typeof gameArray[id] == "undefined") {
             if (firstPlayerTurn) {
-
+                gameArray[id] = markerX;
+                firstPlayerTurn = false
+            } else {
+                gameArray[id] = markerO;
+                firstPlayerTurn = true;
             }
+            reloadGridCells(this, playerTwo);
         }
     }
-    const createGridCells = function() {
+    const createGridCells = function(playerOne, playerTwo) {
         for (let i = 0; i < 9; i++) {
             //create gridcells for the gameboard
             //populate from the gameArray
             div = document.createElement("div");
             div.setAttribute("id", i);
             div.textContent = gameArray[i];
-            div.addEventListener("click", gridCellCallback);
+            div.addEventListener("click", gridCellCallback.bind(playerOne, playerTwo));
             gameGrid.appendChild(div);
         }
     }
-    return {gameArray, createGridCells};
+    const reloadGridCells = function(playerOne, playerTwo) {
+        //delete all gridCells and generate new cells with the current gameArray Items
+        while(gameGrid.firstChild) gameGrid.removeChild(gameGrid.lastChild);
+        createGridCells(playerOne, playerTwo);
+    }
+    return {createGridCells};
 })();
 const displayController = (function() {
     //Module to control the display
-    let playerOne;
-    let playerTwo;
     const toggleVisibility = function(element) {
         //toggle between visible and invisible class
         element.classList.toggle("visible");
@@ -47,15 +59,13 @@ const displayController = (function() {
         toggleVisibility(playerNames);
         firstPlayerDiv.innerText = playerOne.getName();
         secondPlayerDiv.innerText = playerTwo.getName();
-        gameBoard.createGridCells();
-        return {playerOne, playerTwo};
+        gameBoard.createGridCells(playerOne, playerTwo);
     }
     playersForm.onsubmit = formManagement;
     return {};
 })();
 const player = function(name) {
     //Factory function for players
-    let firstPlayer = true;
     const getName = () => name;
     const getMarker = () => {
         let marker;
