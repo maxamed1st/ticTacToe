2eb63e18030fffd194f0abc3e0e8b2e05bb24287
maxamed1st <maxamed1st@icloud.com>
2022-09-02 14:19:36 +0100

    Determine winner and display winner announcement

--- ticTacToe.js
+++ ticTacToe.js
@@ -34,6 +34,7 @@ const gameBoard = (function() {
             div.setAttribute("id", i);
             div.textContent = gameArray[i];
             div.addEventListener("click", gridCellCallback.bind(playerOne, playerTwo));
+            div.addEventListener("click", determineWinner.bind(playerOne, playerTwo));
             gameGrid.appendChild(div);
         }
     }
@@ -42,10 +43,41 @@ const gameBoard = (function() {
         while(gameGrid.firstChild) gameGrid.removeChild(gameGrid.lastChild);
         createGridCells(playerOne, playerTwo);
     }
+    const determineWinner = function(playerTwo) {
+        //check if there is three of the same marker in a row
+        //If so determine winner
+        //this = playerOne
+        if (typeof gameArray[4] != "undefined") {
+            if ((gameArray[4]===gameArray[3] && gameArray[4]===gameArray[5]) || 
+                (gameArray[4]===gameArray[1] && gameArray[4]===gameArray[7]) ||
+                (gameArray[4]===gameArray[2] && gameArray[4]===gameArray[6]) ||
+                (gameArray[4]===gameArray[0] && gameArray[4]===gameArray[8])) {
+                    if (gameArray[4]==="X") displayController.displayWinner(this);
+                    else displayController.displayWinner(playerTwo);
+            }
+        if (typeof gameArray[0] != "undefined") {
+            if ((gameArray[0]===gameArray[1] && gameArray[0]===gameArray[2])||
+                (gameArray[0]==gameArray[3] && gameArray[0]==gameArray[6])) {
+                    if (gameArray[0]==="X") displayController.displayWinner(this);
+                    else displayController.displayWinner(playerTwo);
+            }
+        }
+        if (typeof gameArray[8] != "undefined") {
+            if ((gameArray[8]===gameArray[2] && gameArray[8]===gameArray[5])||
+                (gameArray[8]==gameArray[6] && gameArray[8]==gameArray[7])) {
+                    if (gameArray[8]==="X") displayController.displayWinner(this);
+                    else displayController.displayWinner(playerTwo);
+            }
+        }
+        }
+    }
     return {createGridCells};
 })();
 const displayController = (function() {
     //Module to control the display
+    const model = document.getElementById("model");
+    const winningMessage = document.getElementById("winningMessage");
+    const restart = document.getElementById("restart");
     const toggleVisibility = function(element) {
         //toggle between visible and invisible class
         element.classList.toggle("visible");
@@ -62,7 +94,14 @@ const displayController = (function() {
         gameBoard.createGridCells(playerOne, playerTwo);
     }
     playersForm.onsubmit = formManagement;
-    return {};
+    const displayWinner = function(winner) {
+        //display model and announce the winner
+        const name = winner.getName();
+        console.log(name)
+        winningMessage.innerText = `${name} is the winner of this game`;
+        toggleVisibility(model);
+    }
+    return {displayWinner};
 })();
 const player = function(name) {
     //Factory function for players
